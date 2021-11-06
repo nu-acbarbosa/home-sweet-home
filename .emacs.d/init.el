9f069bf824e80acd60da70087d1e822af00eb070
Alexandre Corrêa Barbosa <alexandre.barbosa@nubank.com.br>
2021-11-06 20:55:33 -0300

    Configures emacs to use straight.el with use-package

---
+++ .emacs.d/init.el
@@ -0,0 +1,23 @@
+;; Disable package.el in favor of straight.el
+(setq package-enable-at-startup nil)
+
+;; Installs straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Install use-package
+(straight-use-package 'use-package)
+
+;; Configures use-package to always use straight.el
+(use-package straight
+  :custom (straight-use-package-by-default t))
