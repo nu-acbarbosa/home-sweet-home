5d6e60cfbbfbc7359581306a876ffbddfab55243
Alexandre Corrêa Barbosa <alexandre.barbosa@nubank.com.br>
2021-11-08 14:59:08 -0300

    Removes menu bar and startup message from emacs

--- .emacs.d/config/look-and-feel.el
+++ .emacs.d/config/look-and-feel.el
@@ -3,3 +3,10 @@
 	     (file-name-as-directory "~/.emacs.d/themes"))
 (load-theme 'acbarbosa-dark t nil)
 (load-theme 'acbarbosa-light t t)
+
+;; Deactivate menu bar
+(menu-bar-mode -1)
+
+;; Deactivating startup message
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
