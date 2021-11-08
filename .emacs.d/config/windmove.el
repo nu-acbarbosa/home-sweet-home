0020945ec87d0121a7af6705080b2b5e35a5c157
Alexandre Corrêa Barbosa <alexandre.barbosa@nubank.com.br>
2021-11-08 12:00:51 -0300

    Configures shortcuts for windmove on emacs

---
+++ .emacs.d/config/windmove.el
@@ -0,0 +1,6 @@
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+
+
