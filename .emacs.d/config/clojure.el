fb637d8f4087f9f254a0c66d551eeeaf38c31fbf
Alexandre Corrêa Barbosa <alexandre.barbosa@nubank.com.br>
2021-11-11 10:37:33 -0300

    Configures hide-show shortcuts for clojure mode

--- .emacs.d/config/clojure.el
+++ .emacs.d/config/clojure.el
@@ -1,4 +1,8 @@
 (use-package clojure-mode
+  :bind (:map clojure-mode-map
+	      ("C-c -" . hs-hide-all)
+	      ("C-c +" . hs-show-all)
+	      ("C-c t" . hs-toggle-hiding))
   :hook ((clojure-mode-hook . linum-mode)
 	 (clojure-mode-hook . hs-minor-mode)
 	 (clojure-mode-hook . lsp))
