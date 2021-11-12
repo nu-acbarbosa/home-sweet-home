827c4376ed97fe7c338ea5495f5d97cc0a20d209
Alexandre Corrêa Barbosa <alexandre.barbosa@nubank.com.br>
2021-11-12 15:11:55 -0300

    Configures hs-minor-mode into typescript-mode

--- .emacs.d/config/typescript.el
+++ .emacs.d/config/typescript.el
@@ -1,5 +1,10 @@
 (use-package typescript-mode
+  :bind (:map typescript-mode-map
+	      ("C-c -" . hs-hide-all)
+	      ("C-c +" . hs-show-all)
+	      ("C-c t" . hs-toggle-hiding))
   :hook ((typescript-mode-hook . lsp)
-	 (typescript-mode-hook . linum-mode))
+	 (typescript-mode-hook . linum-mode)
+	 (typescript-mode-hook . hs-minor-mode))
   :config
   (setq lsp-clients-typescript-tls-path "/Users/alexandre.barbosa/.nvm/versions/node/v16.13.0/bin/typescript-language-server"))
